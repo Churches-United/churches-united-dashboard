33b48ddc5e0e9c001bd8bc728e8b84ed6402734b
NoneTheWeisser <214901165+NoneTheWeisser@users.noreply.github.com>
2026-01-06 16:16:13 -0600

    reports dashboard start

---
+++ src/components/ReportingHub/ReportsDashboard.jsx
@@ -0,0 +1,15 @@
+import VolunteerReportsPage from "../CommunityOutreach/Reports/VolunteerReportsPage";
+import DevelopmentReports from "../Development/DevelopmentReports";
+import HousingReports from "../Housing/HousingReports";
+
+export default function ReportsDashboard() {
+  return (
+    <>
+      <h1>Reporting Hub</h1>
+      <p>work in progress...</p>
+      <VolunteerReportsPage />
+      <DevelopmentReports />
+      <HousingReports />
+    </>
+  );
+}
