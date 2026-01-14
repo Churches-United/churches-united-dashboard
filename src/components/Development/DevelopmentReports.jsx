37fae2316e408db458da47725b9dfb9aabc3eb54
NoneTheWeisser <214901165+NoneTheWeisser@users.noreply.github.com>
2026-01-14 09:16:25 -0600

    added department class names to styles so global styles will be easier to keep

--- src/components/Development/DevelopmentReports.jsx
+++ src/components/Development/DevelopmentReports.jsx
@@ -53,23 +53,3 @@ export default function DevelopmentReports() {
     </div>
   );
 }
-
-// import React from "react";
-// import DonationReporting from "./Reports/DonationReporting";
-// import EventsReporting from "./Reports/EventsReporting";
-
-// export default function DevelopmentReports() {
-//   return (
-//     <>
-//       <h2>Development Reports</h2>
-
-//       <section style={{ marginBottom: "2rem" }}>
-//         <DonationReporting />
-//       </section>
-//       {/* todo - should these two be tabs? */}
-//       <section>
-//         <EventsReporting />
-//       </section>
-//     </>
-//   );
-// }
